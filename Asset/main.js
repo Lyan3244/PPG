438d3a9988b1c54dcfb1c61f8664b9eb54a8d1e8
Lyan3244 <luthfannisapramunegari.xiao@gmail.com>
2024-07-20 23:11:18 +0700

    20 Juli

--- Asset/main.js
+++ Asset/main.js
@@ -34,3 +34,22 @@ function scrollHeader(){
     if(this.scrollY >= 100) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
+
+/*==================== VIDEO ====================*/ 
+const videoFile = document.getElementById('video-file'),
+    videoButton = document.getElementById('video-button')
+    videoIcon = document.getElementById('video-icon')
+
+function playPause(){
+    if(videoFile.paused){
+        videoFile.play()
+        videoIcon.classList.add('ri-pause-line')
+        videoIcon.classList.remove('ri-play-line')
+    } else{
+        videoFile.pause()
+        videoIcon.classList.remove('ri-pause-line')
+        videoIcon.classList.add('ri-play-line')
+    }
+}
+
+videoButton.addEventListener('click', playPause)
